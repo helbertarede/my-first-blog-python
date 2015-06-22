46c7964adfd814c623a474a3c53d4869a5588f4e
= <hsilva.arede@gmail.com>
2015-06-22 13:32:52 -0300

    Implementação do botão de edição.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
 	posts = Post.objects.all()
@@ -14,9 +15,11 @@ def post_detail(request, pk):
 
 def post_new(request):
 	if request.method == "POST":
-		form = PostForm(request.POST)
+		form = PostForm(request.POST, instance=post)
 		if form.is_valid():
 			post = form.save(commit=False)
+			# Resolver problema de segurança
+			#http://tutorial.djangogirls.org/en/django_forms/index.html
 			post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
@@ -24,3 +27,18 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+	
